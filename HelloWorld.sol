3031c0ae688c9efa8d3d9822964be6ffbe1efe22
"temitayopelumi" <"olalekantemitayo7@gmail.com">
2022-03-10 15:50:47 +0100

    First commit

---
+++ HelloWorld.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: UNLICENSED
+
+
+pragma solidity ^0.8.7;
+
+contract HelloWorld {
+    function get()public pure returns (string memory){
+        return 'Hello World';
+    }
+}
